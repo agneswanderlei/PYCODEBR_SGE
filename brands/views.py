80889b956eaf814a65e94bbccd72f139c17bb136
Agnes Wanderlei <agneswanderlei@gmail.com>
2024-08-11 14:27:09 -0300

    finalizei o crud de brands

--- brands/views.py
+++ brands/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from . import models, forms
 
 
@@ -21,4 +21,22 @@ class BrandCreateView(CreateView):
     model = models.Brand
     template_name = 'brand_create.html'
     form_class = forms.BrandForm
+    success_url = reverse_lazy('brand_list')
+
+
+class BrandDetailView(DetailView):
+    model = models.Brand
+    template_name = 'brand_detail.html'
+
+
+class BrandUpdateView(UpdateView):
+    model = models.Brand
+    template_name = 'brand_update.html'
+    success_url = reverse_lazy('brand_list')
+    form_class = forms.BrandForm
+
+
+class BrandDeleteView(DeleteView):
+    model = models.Brand
+    template_name = 'brand_delete.html'
     success_url = reverse_lazy('brand_list')
